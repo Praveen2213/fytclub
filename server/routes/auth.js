297e35e7a14514f3a03be40fd13ebc946eb35b63
Praveen2213 <praveeny2212@gmail.com>
2026-03-28 21:21:24 +0530

    auth complete

---
+++ server/routes/auth.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const pool = require('../db');
+const jwt = require('jsonwebtoken');
+
+router.post('/register', async(req, res) => {
+    const {username, email, password} = req.body;
+    const hashedPassword = await bcrypt.hash(password,10);
+    
+    const result = await pool.query(
+        'INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3) RETURNING id, username, email',  [username, email, hashedPassword]
+    );
+    res.status(201).json(result.rows[0]);
+});
+
+router.post('/login', async(req, res) => {
+    const {email, password} = req.body;
+
+    const result = await pool.query(
+        'SELECT * FROM users WHERE email = $1', [email]
+    );
+
+    if(result.rows.length === 0){
+        return res.status(401).json({message: 'Email not found'});
+    }
+
+    const user = result.rows[0];
+    const isMatch = await bcrypt.compare(password, user.password_hash);
+
+    if(!isMatch){
+        return res.status(401).json({message: 'Incorrect Password'});
+    }
+
+    const token = jwt.sign(
+        {userId: user.id},
+        process.env.JWT_SECRET,
+        {expiresIn: '7d'}
+    );
+
+    res.status(200).json({token});
+});
+
+module.exports = router;
