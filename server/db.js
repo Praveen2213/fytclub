4c318a7b92e314d636eb49d43059e110f3194af9
Praveen2213 <praveeny2212@gmail.com>
2026-05-27 00:35:53 +0530

    fix: update db config for Railway

--- server/db.js
+++ server/db.js
@@ -1,11 +1,8 @@
 const {Pool}  = require('pg');
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  user: process.env.DB_USER
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
