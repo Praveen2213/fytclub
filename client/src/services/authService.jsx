ed7886dd781c65499b82bdfa68d012bbe707ed18
Roshni Panda <roshnipanda2005@gmail.com>
2026-05-28 01:15:01 +0530

    auth

--- client/src/services/authService.jsx
+++ client/src/services/authService.jsx
@@ -1,13 +1,27 @@
 import axios from "axios";
+
 const API = axios.create({
-     baseURL: `${import.meta.env.VITE_API_URL}`,
+  baseURL: `${import.meta.env.VITE_API_URL}/api/auth`,
 });
-export async function loginUser(data){
-    const response = await API.post("/login",data);
-    console.log(response.data);
-    return response.data;
+
+export async function loginUser(data) {
+
+  const response =
+    await API.post(
+      "/login",
+      data
+    );
+
+  return response.data;
 }
-export async function signupUser(data){
-    const response= await API.post("/register",data);
-    return response.data;
+
+export async function signupUser(data) {
+
+  const response =
+    await API.post(
+      "/register",
+      data
+    );
+
+  return response.data;
 }
