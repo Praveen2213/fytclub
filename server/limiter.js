d0d1c1a9e7bcfa921a9b901ecc77c15b38c3bcb0
Praveen2213 <praveeny2212@gmail.com>
2026-05-28 13:32:47 +0530

    req limit increased for testing purpose

--- server/limiter.js
+++ server/limiter.js
@@ -2,13 +2,13 @@ const rateLimit = require('express-rate-limit');
 
 const strictLimit = rateLimit({
     windowMs: 10*60*1000, //10mins
-    max: 5,
+    max: 400,
     message: {message: 'Too many requests, Please try again later'} 
 });
 
 const activityLimit = rateLimit({
     windowMs: 10*60*1000,
-    max: 20,
+    max: 400,
     message: {message: 'Too many requests, Please try again later'}
 });
 
